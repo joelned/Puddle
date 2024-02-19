6fe6bc90c4454835c03d0b016f113a5854769dc3
Joel <joelekwegh3@gmail.com>
2024-02-19 02:12:05 +0100

    Category and Product Models and Views Done

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, OrderItem
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(OrderItem)
--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import Category, OrderItem
 
 # Create your views here.
 
 def index(request): 
-    return render(request, 'core/index.html')
+    ordered_items = OrderItem.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'categories': categories, 
+        'ordered_items': ordered_items,
+    })
 
 
 def contact(request): 
